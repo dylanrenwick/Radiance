53eddcb298fecd02695fcb0e563b01b22687a1f8
dylanrenwick <skidsirongenesisdev@gmail.com>
2020-01-03 12:51:22 -0500

    Added Tilemap

---
+++ DigiSouls/Tile/Tilemap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+using DigiSouls.Components;
+using DigiSouls.Serialization;
+
+namespace DigiSouls.Tile
+{
+    public class Tilemap : Component
+    {
+        [SerializedField]
+        public Texture2D SpriteAtlas { get; set; }
+
+        [SerializedField]
+        public int[,] Map { get; set; }
+
+
+    }
+}
