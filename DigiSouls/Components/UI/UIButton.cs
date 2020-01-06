92174f737e5bac34b6b25800c068fcbece640d8b
dylanrenwick <skidsirongenesisdev@gmail.com>
2020-01-06 11:18:50 -0500

    Added hover highlighting to UIButton

--- DigiSouls/Components/UI/UIButton.cs
+++ DigiSouls/Components/UI/UIButton.cs
@@ -20,20 +20,26 @@ namespace DigiSouls.Components.UI
         [SerializedField]
         public Color TextColor { get; set; }
         [SerializedField]
+        public Color HoverColor { get; set; }
+        [SerializedField]
         public TextHoriAlign HorizontalAlign { get; set; }
         [SerializedField]
         public TextVertAlign VerticalAlign { get; set; }
+
+        private bool isHover;
 
         public UIButton() : base()
         {
             this.HorizontalAlign = TextHoriAlign.Middle;
             this.VerticalAlign = TextVertAlign.Middle;
+            this.HoverColor = new Color(1f, 1f, 1f, 0.3f);
         }
 
         public override void Draw(RenderContext g, GameTime time)
         {
             base.Draw(g, time);
-            g.DrawText(this.Text, new Point((int)this.Transform.Position.X, (int)this.Transform.Position.Y), this.TextColor, 24, this.RectTransform.Rect, this.HorizontalAlign, this.VerticalAlign);
+            if (this.isHover) g.DrawTexture(this.Texture, this.Rect, this.HoverColor);
+            g.DrawText(this.Text, new Point((int)this.Transform.Position.X, (int)this.Transform.Position.Y), this.TextColor, 24, this.Rect, this.HorizontalAlign, this.VerticalAlign);
         }
 
         public override void OnMouseDown(MouseEventArgs e)
@@ -43,6 +49,17 @@ namespace DigiSouls.Components.UI
                 this.OnClick?.Invoke(e);
             }
             base.OnMouseDown(e);
+        }
+
+        public override void OnUIMouseEnter(MouseEventArgs e)
+        {
+            this.isHover = true;
+            base.OnUIMouseEnter(e);
+        }
+        public override void OnUIMouseExit(MouseEventArgs e)
+        {
+            this.isHover = false;
+            base.OnUIMouseExit(e);
         }
     }
 }
