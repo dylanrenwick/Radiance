f5f9efad0e736c0c7c293452879405fc02f3d077
dylanrenwick <skidsirongenesisdev@gmail.com>
2020-01-06 12:01:42 -0500

    Added platform-specific input code for checking if mouse buttons are swapped

--- DigiSouls/Input.cs
+++ DigiSouls/Input.cs
@@ -4,6 +4,7 @@ using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,11 +17,22 @@ namespace DigiSouls
         public event MouseEventHandler OnMouseButtonUp;
         public event MouseEventHandler OnMouseMove;
 
+        public bool MouseButtonsSwapped { get; private set; }
+
         private MouseState previousMouseState;
+
+#if WINDOWS
+        [DllImport("User32.dll", CharSet=CharSet.Auto)]
+        public static extern IntPtr GetSystemMetrics(IntPtr hnewview);
+#endif
 
         public Input()
         {
-
+#if WINDOWS
+            this.MouseButtonsSwapped = GetSystemMetrics(new IntPtr(23)).ToInt32() != 0;
+#elif LINUX
+            this.MouseButtonsSwapped = false;
+#endif
         }
 
         public void Update(GameTime gameTime)
@@ -33,14 +45,14 @@ namespace DigiSouls
             MouseState mouse = Mouse.GetState();
             if (mouse.Equals(this.previousMouseState)) return;
 
-            if (mouse.LeftButton == ButtonState.Pressed)
+            if (this.IsLeftButtonDown(mouse))
             {
-                if (this.previousMouseState.LeftButton == ButtonState.Released)
+                if (!this.IsLeftButtonDown(this.previousMouseState))
                 {
                     this.OnMouseButtonDown?.Invoke(new MouseEventArgs(mouse, this.previousMouseState));
                 }
             }
-            else if (this.previousMouseState.LeftButton == ButtonState.Pressed)
+            else if (this.IsLeftButtonDown(this.previousMouseState))
             {
                 this.OnMouseButtonUp?.Invoke(new MouseEventArgs(mouse, this.previousMouseState));
             }
@@ -51,6 +63,12 @@ namespace DigiSouls
             }
 
             this.previousMouseState = mouse;
+        }
+
+        private bool IsLeftButtonDown(MouseState m)
+        {
+            if (this.MouseButtonsSwapped) return m.RightButton == ButtonState.Pressed;
+            return m.LeftButton == ButtonState.Pressed;
         }
     }
 }
