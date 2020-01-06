ff3d1dbea8ca1efd76d750f4eeabf97f88fb62aa
dylanrenwick <skidsirongenesisdev@gmail.com>
2020-01-06 14:05:31 -0500

    Began work on EntityState system

---
+++ DigiSouls/Components/Entity/EntityStates/EntityState.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace DigiSouls.Components.Entity.EntityStates
+{
+    public abstract class EntityState
+    {
+        private EntityStateMachine parentMachine;
+        protected Entity parentEntity => this.parentMachine.Entity;
+
+        public abstract void Update(GameTime gameTime);
+    }
+}
---
+++ DigiSouls/Components/Entity/EntityStates/EntityStateMachine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace DigiSouls.Components.Entity.EntityStates
+{
+    public class EntityStateMachine : Component, IUpdatable
+    {
+        public Entity Entity => (Entity)this.Parent;
+
+        private EntityState mainState;
+        private EntityState currentState;
+
+        public EntityStateMachine() : base() { }
+        public EntityStateMachine(EntityState mainState) : base()
+        {
+            this.mainState = mainState;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (this.currentState == null) this.currentState = this.mainState;
+            this.currentState.Update(gameTime);
+        }
+    }
+}
