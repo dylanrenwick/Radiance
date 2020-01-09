2ae38f8e9f94c8c2f40a9dac1d761eb395b47973
dylanrenwick <skidsirongenesisdev@gmail.com>
2020-01-09 16:18:35 -0500

    Began work on Config module

---
+++ Radiance/Config/ConfigEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Radiance.Serialization;
+
+namespace Radiance.Config
+{
+    public class ConfigEntry<T>
+    {
+        public string Key { get; private set; }
+        public T Value { get; set; }
+        public ConfigCategory Category { get; private set; }
+
+        public ConfigEntry(string key, ConfigCategory category)
+        {
+            this.Key = key;
+            this.Category = category;
+        }
+
+        public ConfigEntry(JObject obj)
+        {
+            this.Key = obj["Key"].ToString();
+            this.Value = obj["Value"].ToObject<T>();
+        }
+    }
+
+    public enum ConfigCategory
+    {
+        Controls,
+        Graphics,
+        Game,
+        Audio
+    }
+}
---
+++ Radiance/Config/UserConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace Radiance.Config
+{
+    public static class UserConfig
+    {
+        private static Dictionary<ConfigCategory, List<ConfigEntry<object>>> configEntries;
+
+        public static void LoadConfig()
+        {
+            UserConfig.configEntries = new Dictionary<ConfigCategory, List<ConfigEntry<object>>>();
+            foreach (ConfigCategory category in Enum.GetValues(typeof(ConfigCategory)))
+            {
+                UserConfig.configEntries.Add(category, new List<ConfigEntry<object>>());
+            }
+
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/radiance";
+            string configFilePath = appDataPath + "/settings.json";
+        }
+    }
+}
