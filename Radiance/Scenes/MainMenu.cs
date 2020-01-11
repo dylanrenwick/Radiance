165e6de1384dc192b2e4164ca9a5d27476c3274f
Dylan Renwick <skidsirongenesisdev@gmail.com>
2020-01-11 17:14:21 -0500

    Began work on settings screen

--- Radiance/Scenes/MainMenu.cs
+++ Radiance/Scenes/MainMenu.cs
@@ -35,6 +35,21 @@ namespace Radiance.Scenes
 
             playButton.OnClick += _ => SceneManager.SetActiveScene("MainScene");
 
+            UIPanel optOverlay = new UIPanel();
+            optOverlay.Color = new Color(0f, 0f, 0f, 0.3f);
+            optOverlay.RectTransform.Rect = new Rectangle(0, 0, 1280, 720);
+            optOverlay.Active = false;
+
+            UIButton optButton = new UIButton();
+            optButton.Color = new Color(0f, 0f, 0f, 0.3f);
+            optButton.TextColor = Color.White;
+            optButton.Text = "Settings";
+            optButton.RectTransform.Size = new Vector2(buttonSize.X, buttonSize.Y);
+            optButton.Transform.LocalPosition = new Vector3(buttonPos.X, buttonPos.Y, 0);
+            buttonPos.Y += buttonSize.Y + buttonPadding;
+
+            optButton.OnClick += _ => optOverlay.Active = !optOverlay.Active;
+
             UIButton quitButton = new UIButton();
             quitButton.Color = new Color(0f, 0f, 0f, 0.3f);
             quitButton.TextColor = Color.White;
@@ -47,6 +62,8 @@ namespace Radiance.Scenes
 
             mainMenu.AddComponent(title);
             mainMenu.AddComponent(playButton);
+            mainMenu.AddComponent(optOverlay);
+            mainMenu.AddComponent(optButton);
             mainMenu.AddComponent(quitButton);
 
             return mainMenu;
