899d81c1df6259351a0baac1ac5935d25eefcbe5
Dylan Renwick <skidsirongenesisdev@gmail.com>
2020-01-11 18:16:44 -0500

    Added RadianceTest

---
+++ RadianceTest/MainMenu.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+
+using Radiance.Scenes;
+using Radiance.Components.UI;
+
+namespace RadianceTest
+{
+    public static partial class SceneBuilder
+    {
+        public static Scene MainMenu(Point location, Point buttonSize, int buttonPadding)
+        {
+            Scene mainMenu = new Scene("MainMenu");
+
+            Point buttonPos = location;
+
+            UILabel title = new UILabel();
+            title.Text = "Radiance";
+            title.FontSize = 72;
+            title.Color = Color.White;
+            title.RectTransform.Rect = new Rectangle(50, 50, 1000, 150);
+
+            UIButton playButton = new UIButton();
+            playButton.Color = new Color(0f, 0f, 0f, 0.3f);
+            playButton.TextColor = Color.White;
+            playButton.Text = "Play";
+            playButton.RectTransform.Size = new Vector2(buttonSize.X, buttonSize.Y);
+            playButton.Transform.LocalPosition = new Vector3(buttonPos.X, buttonPos.Y, 0);
+            buttonPos.Y += buttonSize.Y + buttonPadding;
+
+            playButton.OnClick += _ => SceneManager.SetActiveScene("MainScene");
+
+            UIPanel optOverlay = new UIPanel();
+            optOverlay.Color = new Color(0f, 0f, 0f, 0.3f);
+            optOverlay.RectTransform.Rect = new Rectangle(0, 0, 1280, 720);
+            optOverlay.Active = false;
+
+            UIButton optButton = new UIButton();
+            optButton.Color = new Color(0f, 0f, 0f, 0.3f);
+            optButton.TextColor = Color.White;
+            optButton.Text = "Settings";
+            optButton.RectTransform.Size = new Vector2(buttonSize.X, buttonSize.Y);
+            optButton.Transform.LocalPosition = new Vector3(buttonPos.X, buttonPos.Y, 0);
+            buttonPos.Y += buttonSize.Y + buttonPadding;
+
+            optButton.OnClick += _ => optOverlay.Active = !optOverlay.Active;
+
+            UIButton quitButton = new UIButton();
+            quitButton.Color = new Color(0f, 0f, 0f, 0.3f);
+            quitButton.TextColor = Color.White;
+            quitButton.Text = "Quit";
+            quitButton.RectTransform.Size = new Vector2(buttonSize.X, buttonSize.Y);
+            quitButton.Transform.LocalPosition = new Vector3(buttonPos.X, buttonPos.Y, 0);
+            buttonPos.Y += buttonSize.Y + buttonPadding;
+
+            quitButton.OnClick += _ => Game1.Instance.Exit();
+
+            mainMenu.AddComponent(title);
+            mainMenu.AddComponent(playButton);
+            mainMenu.AddComponent(optOverlay);
+            mainMenu.AddComponent(optButton);
+            mainMenu.AddComponent(quitButton);
+
+            return mainMenu;
+        }
+    }
+}
---
+++ RadianceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RadianceTest")]
+[assembly: AssemblyProduct("RadianceTest")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("af98e94c-aaf4-4c2d-aab7-20a2e33e17ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
